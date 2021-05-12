fba4035b9b9d6697236064aec273ba36785977d0
marcelodeus98 <marcelodeus98@gmail.com>
2021-05-12 12:03:48 -0300

    add my first commit

---
+++ array_student.js
@@ -0,0 +1,23 @@
+// student and student_two are objects 
+
+let student = {
+    name: 'João',
+    age: 23,
+}
+
+let student_two = {
+    name: 'Marcelo',
+    age: 21,
+}
+
+// students it's an array
+
+let students = [
+    student,
+    student_two
+]
+
+//let teste = typeof student 
+
+console.log(students)
+
---
+++ functions.js
@@ -0,0 +1,16 @@
+// estudo das funções - study of functions 
+
+// Para que serve uma function ? - what are the functions for ?
+// A ideia básica em utlilzar funções é para encapsular um código podendo assim utiliza lo varias vezes.
+// The basic idea in using functions is to encapsulate a code so you can use it several times. 
+
+// Exemplo - Example:
+
+// declaration : 
+function createPhrases(){
+    console.log("Estudar é muito bom!")
+    console.log("Continue estudando!")
+    console.log("Você vai longe")
+}
+
+createPhrases()
