d9796a941951aa5a156ff5bd3c0770382ab7a0cd
marcelodeus98 <marcelodeus98@gmail.com>
2022-06-18 09:12:15 -0300

    study filter,map,forEach and reduce

--- variables.js
+++ variables.js
@@ -1,5 +1,5 @@
 // use var 
-var test = 'hi';
+var text = 'hi';
 
 var name = 'Marcelo';
 
@@ -7,8 +7,17 @@ console.log(`My name is ${name}`);
 
 // use let
 // - Ao utilizar o let, existe um bloco com escopo
-let test2  = 'hi';
+let text2  = 'hi';
 
 // user const
 // - Ao utilizar o const não posso alterar o valor.
-const test3 = 'hi';
+const text3 = 'hi';
+
+let test1 = '2'; // string
+let test2 = '2.0'; // string
+let test3 = 2; // int
+let test4 = 2.0; // float 
+let test5 = false; // booleano
+let test6 = true; // booleano
+let test8 = 'Karla' // string
+let test9 = test1 === test3; // booleano
